18b1e032296b2a86445a7531ec8646e7f406d5a4
lambbross <lambbross@gmail.com>
2026-06-03 00:51:44 +0200

    Version 13

---
+++ README.txt
@@ -0,0 +1,1 @@
+Versión v13: eliminados botones duplicados del bloque de inicio; se mantiene Próximos partidos y Automático.
---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>e.waitUntil(caches.open('mi-world-cup-2026-v13').then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./assets/logoblancotransparente.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
